2c1d971fd75f8726512c1d55704f9c5c5abd8a6e
Adam Tahiri <75190352+AdamTahiri@users.noreply.github.com>
2021-05-22 16:08:54 -0400

    Create Home Page (#7)
    * add api func
    
    * create list of videos
    
    * add Video comp
    
    * deleted homepage comp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import About from "./components/About"
 import Navbar from "./components/Navbar"
+import Home from './components/Home';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route path={"/youtube"} component={Homepage} />
+        <Route path={"/youtube"} component={Home} />
         <Route path={"/about"} component={About} />
       </Switch>
     </div>
