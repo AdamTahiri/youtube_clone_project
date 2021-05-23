f3b9f862f99ff2a58a444b0e4d327724d08f0ccf
Kobe <tahiriadam@gmail.com>
2021-05-22 22:27:13 -0400

    create video component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,54 +1,73 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-import axios from 'axios'
-import './Home.css'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./Home.css";
 
 const Home = () => {
-    const [input, setInput] = useState("");
-    const [videosArray, setVideosArray] = useState([]);
-    const [showVideos, setShowVideos] = useState(false);
+  const [input, setInput] = useState("");
+  const [videosArray, setVideosArray] = useState([]);
+  const [showVideos, setShowVideos] = useState(false);
 
-    const fetchYoutubeVideos = async () => {
-        try {
-            const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/search/?part=snippet&maxResults=12&key=${process.env.REACT_APP_API_KEY }&q=${input}`);
-            setVideosArray(res.data.items)
-            debugger
-        } catch (error) {
-            console.log(error);
-            setVideosArray([])
-        }
+  const fetchYoutubeVideos = async () => {
+    try {
+      const res = await axios.get(
+        `https://youtube.googleapis.com/youtube/v3/search/?part=snippet&maxResults=12&key=${process.env.REACT_APP_API_KEY}&q=${input}`
+      );
+      setVideosArray(res.data.items);
+      debugger;
+    } catch (error) {
+      console.log(error);
+      setVideosArray([]);
     }
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        fetchYoutubeVideos();
-        setShowVideos(true)
-        setInput("")
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchYoutubeVideos();
+    setShowVideos(true);
+    setInput("");
+  };
 
-    const handleChange = (e) => {
-        setInput(e.target.value);
-    }
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-    const toggleVideos = () => {
-        setShowVideos(!showVideos);
-    }
+  const toggleVideos = () => {
+    setShowVideos(!showVideos);
+  };
 
-    return (
-        <section>
-            <form onSubmit={handleSubmit}>
-            <input type="text" onChange={handleChange} value={input} placeholder="search"/>
-            <button>Search</button>
-            </form>
-            {showVideos ? 
-            <ul>
-            {videosArray.map((video) => {
-                return <li key={video.id.videoId}> <img src={video.snippet.thumbnails.default.url}/><b>{video.snippet.title}</b> <br/>{video.snippet.description}</li>
-            })}
-            </ul> : "No Videos" 
-            }
-        </section>
-    )
-}
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={input}
+          placeholder="search"
+        />
+        <button>Search</button>
+      </form>
+      {showVideos ? (
+        <ul>
+          {videosArray.map((video) => {
+            return (
+              <li key={video.id.videoId}>
+                <Link to={`/video/${video.id.videoId}`}>
+                {" "}
+                <img src={video.snippet.thumbnails.default.url} />
+                <b>{video.snippet.title}</b> <br />
+                {video.snippet.description}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        "No Videos"
+      )}
+    </section>
+  );
+};
 
-export default Home
+export default Home;
--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useParams, Link } from "react-router-dom";
 
 const Video = () => {
+  const { id } = useParams();
   return (
     <div>
+      <iframe
+        width="420"
+        height="315"
+        src={`https://www.youtube.com/embed/${id}`}
+      ></iframe>
+      <Link to="/youtube">Go Back</Link>
     </div>
   );
 };
