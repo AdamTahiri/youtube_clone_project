5fdf10aea14d26bf275b7622a1bf14a682692019
Adam Tahiri <75190352+AdamTahiri@users.noreply.github.com>
2021-05-26 21:10:58 -0400

    create video component (#10)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import About from "./components/About"
 import Navbar from "./components/Navbar"
+import Home from './components/Home';
+import Video from './components/Video';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route path={"/youtube"} component={Homepage} />
+        <Route path={"/youtube"} component={Home} />
         <Route path={"/about"} component={About} />
+        <Route path={"/video/:id"} component={Video} />
       </Switch>
     </div>
   );
